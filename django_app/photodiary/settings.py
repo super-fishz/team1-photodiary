a22621c5b97ed5636a7a60be3ea27db4a62adc72
yusunglee2074 <lys0333@gmail.com>
2016-12-01 18:40:32 +0900

    오타수정

--- django_app/photodiary/settings.py
+++ django_app/photodiary/settings.py
@@ -158,8 +158,8 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 STATICFILES_LOCATION = 'static'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
-STATICFILES_STORAGE = 'photodiary.custom_storages.StaticStorage'
+STATICFILES_STORAGE = 'photo.custom_storages.StaticStorage'
 
 MEDIAFILES_LOCATION = 'media'
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-DEFAULT_FILE_STORAGE = 'photodiary.custom_storages.MediaStorage'
+DEFAULT_FILE_STORAGE = 'photo.custom_storages.MediaStorage'
