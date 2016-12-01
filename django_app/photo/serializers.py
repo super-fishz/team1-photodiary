7aa85b533de46d453eba1c51bcc1a145f28e6f04
yusunglee2074 <lys0333@gmail.com>
2016-12-01 21:41:12 +0900

    보기 편하게 코드 수정

--- django_app/photo/serializers.py
+++ django_app/photo/serializers.py
@@ -5,4 +5,5 @@ from .models import Photo
 class Photoserializer(ModelSerializer):
     class Meta:
         model = Photo
-        fields = ('title', 'author', 'content', 'modified_date', 'created_date', 'image', 'post')
+        fields = ('title', 'author', 'content',
+                  'modified_date', 'created_date', 'image', 'post')
