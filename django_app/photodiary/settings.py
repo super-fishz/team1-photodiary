46b0cac04826c8905c70afa364a78c302fd711f3
yusunglee2074 <lys0333@gmail.com>
2016-12-07 16:13:15 +0900

    재 마이그레이션, 디버그 False로 변경

--- django_app/photodiary/settings.py
+++ django_app/photodiary/settings.py
@@ -17,14 +17,9 @@ import json
 import sys
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-MEDIA_URL = '/media/'
-
-
-# ROOT_DIR = os.path.dirname(BASE_DIR)
-# CONF_DIR = os.path.join(ROOT_DIR, '.django-conf')
-# STATIC_ROOT = os.path.join(ROOT_DIR, 'static_root')
+ROOT_DIR = os.path.dirname(BASE_DIR)
+CONF_DIR = os.path.join(ROOT_DIR, '.django-conf')
+STATIC_ROOT = os.path.join(ROOT_DIR, 'static_root')
 
 
 # Quick-start development settings - unsuitable for production
@@ -34,16 +29,16 @@ MEDIA_URL = '/media/'
 SECRET_KEY = '0evi3yah))cipdsq-9y-ys109yyzjmlgo_p9&dtzj)*5v!$9o%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # DEBUG = (len(sys.argv) > 1 and sys.argv[1] == 'runserver')
          # or sys.argv[1] == 'makemigrations' or sys.argv[1] == 'migrate' )
 
 
 # 디버그 여부에 따라 config 파일 다르게 불러오기
-# if DEBUG:
-#     config = json.loads(open(os.path.join(CONF_DIR, 'settings_debug.json')).read())
-# else:
-#     config = json.loads(open(os.path.join(CONF_DIR, 'settings_deploy.json')).read())
+if DEBUG:
+    config = json.loads(open(os.path.join(CONF_DIR, 'settings_debug.json')).read())
+else:
+    config = json.loads(open(os.path.join(CONF_DIR, 'settings_deploy.json')).read())
 
 ALLOWED_HOSTS = [
     '.elasticbeanstalk.com',
@@ -104,22 +99,15 @@ WSGI_APPLICATION = 'photodiary.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 
-# if DEBUG:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
-# else:
-#     DATABASES = config['databases']
-
-DATABASES = {
+if DEBUG:
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
+else:
+    DATABASES = config['databases']
 
 
 # Password validation
@@ -170,26 +158,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-
-
-STATIC_URL = '/static/'
-
-# AWS_HEADERS = {
-#     'Expires': 'Thu, 31 Dec 2199 20:00:00 GMT',
-#     'Cache-Control': 'max-age=94608000',
-# }
-# AWS_STORAGE_BUCKET_NAME = config['aws']['AWS_STORAGE_BUCKET_NAME']
-# AWS_ACCESS_KEY_ID = config['aws']['AWS_ACCESS_KEY_ID']
-# AWS_SECRET_ACCESS_KEY = config['aws']['AWS_SECRET_ACCESS_KEY']
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-#
-# STATICFILES_LOCATION = 'static'
-# STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
-# STATICFILES_STORAGE = 'post.custom_storages.StaticStorage'
-#
-# MEDIAFILES_LOCATION = 'media'
-# MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-# DEFAULT_FILE_STORAGE = 'post.custom_storages.MediaStorage'
 
 
 
